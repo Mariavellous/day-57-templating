5925017db86b8e92f003b1a5a37b151bc37d83d0
Mariavellous <mm.alcaide38@gmail.com>
2021-09-26 22:27:25 -0700

    templating with jinja

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import random
+from datetime import date
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+  random_number = random.randint(1, 10)
+  current_year = date.today().year
+  return render_template("index.html", num=random_number, YYYY=current_year)
+
+
+# makes a request to the website and get its json data with a specific key
+def get_data(url, key):
+  response = requests.get(url=url)
+  data = response.json()
+  info = data[key]
+  return info
+
+
+@app.route("/guess/<user_name>")
+def guess(user_name):
+  username = user_name.title()
+  gender_url = f"https://api.genderize.io?name={user_name}"
+  gender = get_data(gender_url, "gender")
+
+  age_url = f'https://api.agify.io?name={user_name}'
+  age = get_data(age_url, "age")
+
+  return render_template("guess.html", name=username, gender=gender, age=age)
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
+
+
